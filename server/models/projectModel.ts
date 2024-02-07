bdde4e3740ad63416dba311ffbdde2eb71aff14c
David <23064280+itsdavehimself@users.noreply.github.com>
2024-02-07 12:32:38 -0600

    Add timestamps key

--- server/models/projectModel.ts
+++ server/models/projectModel.ts
@@ -12,21 +12,23 @@ interface Project {
 
 export interface ProjectDocument extends Project, Document {}
 
-const projectSchema = new Schema<ProjectDocument>({
-  author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  title: {
-    type: String,
-    required: true,
-    trim: true,
+const projectSchema = new Schema<ProjectDocument>(
+  {
+    author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    tags: [{ type: String, required: true }],
+    software: [{ type: String, required: true }],
+    likes: { type: Number, default: 0 },
   },
-  description: {
-    type: String,
-    required: false,
-  },
-  tags: [{ type: String, required: true }],
-  software: [{ type: String, required: true }],
-  likes: { type: Number, default: 0 },
-  createdAt: { type: Date, default: Date.now },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model<ProjectDocument>('Project', projectSchema);
