dbb9470149cd90c605e765fe4cbd86d0727aa616
David <23064280+itsdavehimself@users.noreply.github.com>
2024-02-11 09:12:22 -0600

    Create action types

---
+++ client/src/types/ActionTypes.ts
@@ -0,0 +1,13 @@
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+
+interface LoginAction {
+  type: typeof LOGIN;
+  payload: { email: string; token: string };
+}
+
+interface LogoutAction {
+  type: typeof LOGOUT;
+}
+
+export type AuthAction = LoginAction | LogoutAction;
