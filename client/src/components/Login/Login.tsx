21303fa4bd9bd45c4ac156946abcee84196b8f99
David <23064280+itsdavehimself@users.noreply.github.com>
2024-02-11 09:14:03 -0600

    Create login component

---
+++ client/src/components/Login/Login.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import useLogin from '../../hooks/useLogin';
+
+const Login: React.FC = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const { login, error, isLoading } = useLogin();
+
+  const handleSubmitLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    await login(email, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmitLogin}>
+      <h3>Sign up</h3>
+      <label htmlFor="email">Email:</label>
+      <input type="email" onChange={(e) => setEmail(e.target.value)} />
+      <label htmlFor="password">Password:</label>
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />{' '}
+      <button disabled={isLoading}>Sign Up</button>
+      {error && <div>{error.toString()}</div>}
+    </form>
+  );
+};
+
+export default Login;
