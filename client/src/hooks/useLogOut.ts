b68db3e4e86b59dba4654e919a8a0143a0cd7f84
David <23064280+itsdavehimself@users.noreply.github.com>
2024-02-11 09:13:07 -0600

    Create logout hook

---
+++ client/src/hooks/useLogOut.ts
@@ -0,0 +1,19 @@
+import { useAuthContext } from './useAuthContext';
+
+interface LogOutResult {
+  logOut: () => void;
+}
+
+const useLogOut = (): LogOutResult => {
+  const { dispatch } = useAuthContext();
+
+  const logOut = () => {
+    localStorage.removeItem('user');
+  };
+
+  dispatch({ type: 'LOGOUT' });
+
+  return { logOut };
+};
+
+export default useLogOut;
