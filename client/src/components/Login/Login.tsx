4a135f1858580e4b2389fa5ab61d454cdc639be6
David <23064280+itsdavehimself@users.noreply.github.com>
2024-02-12 21:09:19 -0600

    Add more form components

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import useLogin from '../../hooks/useLogin';
+import styles from './Login.module.scss';
+import { Link } from 'react-router-dom';
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState<string>('');
@@ -14,18 +16,63 @@ const Login: React.FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmitLogin}>
-      <h3>Login</h3>
-      <label htmlFor="email">Email:</label>
-      <input type="email" onChange={(e) => setEmail(e.target.value)} />
-      <label htmlFor="password">Password:</label>
-      <input
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-      />{' '}
-      <button disabled={isLoading}>Login</button>
-      {error && <div>{error.toString()}</div>}
-    </form>
+    <div className={styles['login-container']}>
+      <div className={styles['form-container']}>
+        <div className={styles['form-image']}>
+          <h3 className={styles['form-copy-large']}>
+            EXPERIENCE THE FUTURE OF AI-DRIVEN EXPRESSION
+          </h3>
+          <p className={styles['form-copy-small']}>
+            Discover a haven for AI art enthusiasts. Share your projects,
+            exchange ideas, and connect with a community that values the
+            precision and depth of your creative process.
+          </p>
+        </div>
+        <div className={styles['form-elements']}>
+          <form onSubmit={handleSubmitLogin}>
+            <h1 className={styles['login-header']}>Log In</h1>
+            <div className={styles['input-container']}>
+              <label htmlFor="email">Email</label>
+              <input type="email" onChange={(e) => setEmail(e.target.value)} />
+            </div>
+            <div className={styles['input-container']}>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button className={styles['signup-button']} disabled={isLoading}>
+              Log In
+            </button>
+            {error && <div>{error.toString()}</div>}
+          </form>
+          <div className={styles.divider}>
+            <div className={styles['divider-line']}></div>
+            <span className={styles['divider-text']}>or</span>
+            <div className={styles['divider-line']}></div>
+          </div>
+          <div className={styles['oauth-buttons']}>
+            <button className={styles['oauth-button']}>
+              Continue with Google
+            </button>
+            <button className={styles['oauth-button']}>
+              Continue with Facebook
+            </button>
+          </div>
+          <div className={styles['members-container']}>
+            Not a member yet?{' '}
+            <Link className={styles.link} to="/signup">
+              Sign Up
+            </Link>
+          </div>
+          <div className={styles['terms-container']}>
+            By logging in to RenderCove, I confirm that I have read and agree to
+            the RenderCove Terms of Service and Privacy Policy.
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- client/src/components/SignUp/SignUp.tsx
+++ client/src/components/SignUp/SignUp.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import useSignUp from '../../hooks/useSignUp';
+import styles from './SignUp.module.scss';
+import { Link } from 'react-router-dom';
 
 const SignUp: React.FC = () => {
   const [email, setEmail] = useState<string>('');
@@ -13,33 +15,80 @@ const SignUp: React.FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmitSignUp}>
-      <h3>Sign up</h3>
-      <label htmlFor="email">Email:</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        autoComplete="true"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <label htmlFor="password">Password:</label>
-      <input
-        id="password"
-        name="password"
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-      />{' '}
-      <label htmlFor="displayName">Display Name:</label>
-      <input
-        id="displayName"
-        name="displayName"
-        type="text"
-        onChange={(e) => setDisplayName(e.target.value)}
-      />
-      <button disabled={isLoading}>Sign Up</button>
-      {error && <div>{error?.toString()}</div>}
-    </form>
+    <div className={styles['signup-container']}>
+      <div className={styles['form-container']}>
+        <div className={styles['form-image']}>
+          <h3 className={styles['form-copy-large']}>
+            EXPERIENCE THE FUTURE OF AI-DRIVEN EXPRESSION
+          </h3>
+          <p className={styles['form-copy-small']}>
+            Discover a haven for AI art enthusiasts. Share your projects,
+            exchange ideas, and connect with a community that values the
+            precision and depth of your creative process.
+          </p>
+        </div>
+        <div className={styles['form-elements']}>
+          <h1 className={styles['signup-header']}>Join RenderCove</h1>
+          <div className={styles['oauth-buttons']}>
+            <button className={styles['oauth-button']}>
+              Continue with Google
+            </button>
+            <button className={styles['oauth-button']}>
+              Continue with Facebook
+            </button>
+          </div>
+          <div className={styles.divider}>
+            <div className={styles['divider-line']}></div>
+            <span className={styles['divider-text']}>or</span>
+            <div className={styles['divider-line']}></div>
+          </div>
+          <form onSubmit={handleSubmitSignUp}>
+            <div className={styles['input-container']}>
+              <label htmlFor="email">Add your email</label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="true"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className={styles['input-container']}>
+              <label htmlFor="displayName">Choose a display name</label>
+              <input
+                id="displayName"
+                name="displayName"
+                type="text"
+                onChange={(e) => setDisplayName(e.target.value)}
+              />
+            </div>
+            <div className={styles['input-container']}>
+              <label htmlFor="password">Create a password</label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button className={styles['signup-button']} disabled={isLoading}>
+              Sign Up
+            </button>
+            {error && <div>{error?.toString()}</div>}
+          </form>
+          <div className={styles['members-container']}>
+            Already a member?{' '}
+            <Link className={styles.link} to="/login">
+              Log In
+            </Link>
+          </div>
+          <div className={styles['terms-container']}>
+            By joining RenderCove, I confirm that I have read and agree to the
+            RenderCove Terms of Service and Privacy Policy.
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
