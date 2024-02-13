b366cd70dc5e14df06d89121cf9eb9139287a0f8
David <23064280+itsdavehimself@users.noreply.github.com>
2024-02-12 21:10:41 -0600

    Add comments property

--- server/types/ProjectDocument.ts
+++ server/types/ProjectDocument.ts
@@ -1,13 +1,15 @@
 import { Schema } from 'mongoose';
 import Image from './Image';
+import Comment from './Comment';
 
 interface ProjectDocument {
-  author: Schema.Types.ObjectId | string;
+  author: Schema.Types.ObjectId;
   title: string;
   description?: string;
   tags: string[];
   softwareList: string[];
   likes: number;
+  comments: Comment[];
   images: Image[];
   createdAt: Date;
   updatedAt: Date;
