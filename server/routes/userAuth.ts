3a9be2492a52b9df36759998b85471cec71dad2f
David <23064280+itsdavehimself@users.noreply.github.com>
2024-02-13 14:25:14 -0600

    Create oauth route

--- server/routes/userAuth.ts
+++ server/routes/userAuth.ts
@@ -1,9 +1,14 @@
 import express, { Router } from 'express';
-import { loginUser, signupUser } from '../controllers/userAuthController.js';
+import {
+  loginUser,
+  signupUser,
+  signUpWithOAuth,
+} from '../controllers/userAuthController.js';
 
 const userAuthRouter: Router = express.Router();
 
 userAuthRouter.post('/login', loginUser);
 userAuthRouter.post('/signup', signupUser);
+userAuthRouter.post('/google-oauth', signUpWithOAuth);
 
 export default userAuthRouter;
