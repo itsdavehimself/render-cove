c9d667a5f54a8df5146fa84e3cfe3ef2c5e70c32
David <23064280+itsdavehimself@users.noreply.github.com>
2024-02-15 15:02:18 -0600

    Create pop out menu

---
+++ client/src/components/PopOutMenu/PopOutMenu.tsx
@@ -0,0 +1,24 @@
+import styles from './PopOutMenu.module.scss';
+
+interface PopOutMenuProps {
+  buttons: { icon: React.ReactNode; label: string; onClick: () => void }[];
+}
+
+const PopOutMenu: React.FC<PopOutMenuProps> = ({ buttons }) => {
+  return (
+    <div className={styles['popout-menu']}>
+      {buttons.map((button, index) => (
+        <button
+          className={styles['popout-button']}
+          onClick={button.onClick}
+          key={index}
+        >
+          <div className={styles['popout-button-icon']}>{button.icon}</div>
+          <div>{button.label}</div>
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default PopOutMenu;
