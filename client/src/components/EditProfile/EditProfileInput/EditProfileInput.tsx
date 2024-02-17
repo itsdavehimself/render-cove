f66173f92a7f09f206666e3d2526e669c6024ef9
David <23064280+itsdavehimself@users.noreply.github.com>
2024-02-17 12:04:10 -0600

    Create edit profile input component

---
+++ client/src/components/EditProfile/EditProfileInput/EditProfileInput.tsx
@@ -0,0 +1,53 @@
+import { ChangeEvent } from 'react';
+import styles from './EditProfileInput.module.scss';
+
+interface EditProfileInputProps {
+  htmlFor: string;
+  label: string;
+  type: string;
+  id: string;
+  name: string;
+  value: string;
+  placeholder?: string;
+  clientError?: string;
+  serverError?: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const EditProfileInput: React.FC<EditProfileInputProps> = ({
+  htmlFor,
+  label,
+  type,
+  id,
+  name,
+  value,
+  placeholder,
+  clientError,
+  serverError,
+  onChange,
+}) => {
+  return (
+    <div className={styles['input-container']}>
+      <label className={styles['edit-profile-label']} htmlFor={htmlFor}>
+        {label}
+      </label>
+      <input
+        className={`${styles['edit-profile-input']} ${clientError ? styles.error : ''}`}
+        type={type}
+        id={id}
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+      {clientError && (
+        <div className={styles['input-error-message']}>{clientError}</div>
+      )}
+      {!clientError && serverError && (
+        <div className={styles['input-error-message']}>{serverError}</div>
+      )}
+    </div>
+  );
+};
+
+export default EditProfileInput;
