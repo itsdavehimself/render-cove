5b8a8ef3d731494636cc6fbb787995e93e273615
David <23064280+itsdavehimself@users.noreply.github.com>
2024-02-19 17:23:29 -0600

    Create basic routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,65 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './containers/Home/Home';
+import SignUp from './containers/SignUp/SignUp';
+import Login from './containers/Login/Login';
+import CreateProjectForm from './components/CreateProjectForm/CreateProjectForm';
+import { useAuthContext } from './hooks/useAuthContext';
+import { useEffect, useState } from 'react';
+import WithNav from './components/WithNav/WithNav';
+import WithoutNav from './components/WithoutNav/WithoutNav';
+import EditProfile from './containers/EditProfile/EditProfile';
+import ChooseUsername from './containers/ChooseUsername/ChooseUsername';
+
+const API_BASE_URL: string =
+  import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000';
 
 function App() {
-  return <>Welcome</>;
+  const { user } = useAuthContext();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, [user]);
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        {!isLoading ? (
+          <>
+            <Routes>
+              <Route element={<WithoutNav />}>
+                <Route
+                  path="/signup"
+                  element={!user ? <SignUp /> : <Navigate to="/" />}
+                />
+                <Route
+                  path="/login"
+                  element={!user ? <Login /> : <Navigate to="/" />}
+                />
+                <Route
+                  path="/signup/username"
+                  element={!user ? <ChooseUsername /> : <Navigate to="/" />}
+                />
+              </Route>
+              <Route element={<WithNav />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/profile/edit" element={<EditProfile />} />
+                <Route
+                  path="/create"
+                  element={
+                    user ? <CreateProjectForm /> : <Navigate to="/login" />
+                  }
+                />
+              </Route>
+            </Routes>
+          </>
+        ) : (
+          <div>Loading....</div>
+        )}
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
