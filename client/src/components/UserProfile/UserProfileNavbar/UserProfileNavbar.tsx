380855ca2f15cefffd45a3f15b4ae36f498ba8fc
David <23064280+itsdavehimself@users.noreply.github.com>
2024-02-23 16:18:18 -0600

    Create user profile navbar component

---
+++ client/src/components/UserProfile/UserProfileNavbar/UserProfileNavbar.tsx
@@ -0,0 +1,44 @@
+import styles from './UserProfileNavbar.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faImage,
+  faFlask,
+  faBookmark,
+  faBookOpen,
+} from '@fortawesome/free-solid-svg-icons';
+
+interface UserProfileNavbarProps {}
+
+const UserProfileNavbar: React.FC<UserProfileNavbarProps> = () => {
+  const projectsIcon: React.ReactNode = <FontAwesomeIcon icon={faImage} />;
+  const caseStudyIcon: React.ReactNode = <FontAwesomeIcon icon={faFlask} />;
+  const collectionsIcon: React.ReactNode = (
+    <FontAwesomeIcon icon={faBookmark} />
+  );
+  const tutorialsIcon: React.ReactNode = <FontAwesomeIcon icon={faBookOpen} />;
+
+  return (
+    <nav className={styles['user-profile-navbar']}>
+      <div className={styles['user-nav-item']}>
+        <div className={styles['user-nav-icon']}>{projectsIcon}</div>
+        Projects
+        <div className={styles['user-nav-number']}>0</div>
+      </div>
+      <div className={styles['user-nav-item']}>
+        <div className={styles['user-nav-icon']}>{caseStudyIcon}</div>Case
+        Studies
+        <div className={styles['user-nav-number']}>0</div>
+      </div>
+      <div className={styles['user-nav-item']}>
+        <div className={styles['user-nav-icon']}>{collectionsIcon}</div>
+        Collections
+        <div className={styles['user-nav-number']}>0</div>
+      </div>
+      <div className={styles['user-nav-item']}>
+        <div className={styles['user-nav-icon']}>{tutorialsIcon}</div>Tutorials
+        <div className={styles['user-nav-number']}>0</div>
+      </div>
+    </nav>
+  );
+};
+export default UserProfileNavbar;
