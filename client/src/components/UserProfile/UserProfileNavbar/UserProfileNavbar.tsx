632da63e8847560e8eb2a73b6ed73c292f75c09d
David <23064280+itsdavehimself@users.noreply.github.com>
2024-02-24 13:50:00 -0600

    Render conditional styles for navbar items

--- client/src/components/UserProfile/UserProfileNavbar/UserProfileNavbar.tsx
+++ client/src/components/UserProfile/UserProfileNavbar/UserProfileNavbar.tsx
@@ -7,14 +7,27 @@ import {
   faBookmark,
   faBookOpen,
 } from '@fortawesome/free-solid-svg-icons';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 interface UserProfileNavbarProps {
   username: string | undefined;
 }
 
+enum Views {
+  Latest = '',
+  Projects = 'projects',
+  CaseStudies = 'case-studies',
+  Tutorials = 'tutorials',
+  Collections = 'collections',
+}
+
 const UserProfileNavbar: React.FC<UserProfileNavbarProps> = ({ username }) => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const [currentView, setCurrentView] = useState<Views>(Views.Latest);
+
   const latestIcon: React.ReactNode = <FontAwesomeIcon icon={faBolt} />;
   const projectsIcon: React.ReactNode = <FontAwesomeIcon icon={faImage} />;
   const caseStudyIcon: React.ReactNode = <FontAwesomeIcon icon={faFlask} />;
@@ -23,43 +36,67 @@ const UserProfileNavbar: React.FC<UserProfileNavbarProps> = ({ username }) => {
   );
   const tutorialsIcon: React.ReactNode = <FontAwesomeIcon icon={faBookOpen} />;
 
-  const currentPath: string = `/user/${username}`;
+  const baseUrlParams: string = `/user/${username}`;
+
+  useEffect(() => {
+    const currentLocation = location.pathname.replace(`/user/${username}/`, '');
+
+    const mapLocationToView = (location: string): Views => {
+      switch (location) {
+        case '':
+          return Views.Latest;
+        case 'projects':
+          return Views.Projects;
+        case 'case-studies':
+          return Views.CaseStudies;
+        case 'tutorials':
+          return Views.Tutorials;
+        case 'collections':
+          return Views.Collections;
+        default:
+          return Views.Latest;
+      }
+    };
+
+    const mappedView = mapLocationToView(currentLocation);
+    setCurrentView(mappedView);
+  }, [location.pathname, username]);
 
   return (
     <nav className={styles['user-profile-navbar']}>
       <div
-        className={styles['user-nav-item']}
-        onClick={() => navigate(`${currentPath}/`)}
+        className={`${styles['user-nav-item']} ${currentView === Views.Latest ? styles['current-item'] : ''}`}
+        onClick={() => navigate(`${baseUrlParams}/`)}
       >
         <div className={styles['user-nav-icon']}>{latestIcon}</div>
         Latest
       </div>
       <div
-        className={styles['user-nav-item']}
-        onClick={() => navigate(`${currentPath}/projects`)}
+        className={`${styles['user-nav-item']} ${currentView === Views.Projects ? styles['current-item'] : ''}`}
+        onClick={() => navigate(`${baseUrlParams}/projects`)}
       >
         <div className={styles['user-nav-icon']}>{projectsIcon}</div>
         Projects
         <div className={styles['user-nav-number']}>0</div>
       </div>
       <div
-        className={styles['user-nav-item']}
-        onClick={() => navigate(`${currentPath}/case-studies`)}
+        className={`${styles['user-nav-item']} ${currentView === Views.CaseStudies ? styles['current-item'] : ''}`}
+        onClick={() => navigate(`${baseUrlParams}/case-studies`)}
       >
         <div className={styles['user-nav-icon']}>{caseStudyIcon}</div>Case
         Studies
         <div className={styles['user-nav-number']}>0</div>
       </div>
       <div
-        className={styles['user-nav-item']}
-        onClick={() => navigate(`${currentPath}/tutorials`)}
+        className={`${styles['user-nav-item']} ${currentView === Views.Tutorials ? styles['current-item'] : ''}`}
+        onClick={() => navigate(`${baseUrlParams}/tutorials`)}
       >
         <div className={styles['user-nav-icon']}>{tutorialsIcon}</div>Tutorials
         <div className={styles['user-nav-number']}>0</div>
       </div>
       <div
-        className={styles['user-nav-item']}
-        onClick={() => navigate(`${currentPath}/collections`)}
+        className={`${styles['user-nav-item']} ${currentView === Views.Collections ? styles['current-item'] : ''}`}
+        onClick={() => navigate(`${baseUrlParams}/collections`)}
       >
         <div className={styles['user-nav-icon']}>{collectionsIcon}</div>
         Collections
