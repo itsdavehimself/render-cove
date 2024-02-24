2330a38ca5305f81c3287da27df145cfa1f081f8
David <23064280+itsdavehimself@users.noreply.github.com>
2024-02-23 18:22:44 -0600

    Create navigation to user profile components

--- client/src/components/UserProfile/UserProfileNavbar/UserProfileNavbar.tsx
+++ client/src/components/UserProfile/UserProfileNavbar/UserProfileNavbar.tsx
@@ -7,10 +7,14 @@ import {
   faBookmark,
   faBookOpen,
 } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
-interface UserProfileNavbarProps {}
+interface UserProfileNavbarProps {
+  username: string | undefined;
+}
 
-const UserProfileNavbar: React.FC<UserProfileNavbarProps> = () => {
+const UserProfileNavbar: React.FC<UserProfileNavbarProps> = ({ username }) => {
+  const navigate = useNavigate();
   const latestIcon: React.ReactNode = <FontAwesomeIcon icon={faBolt} />;
   const projectsIcon: React.ReactNode = <FontAwesomeIcon icon={faImage} />;
   const caseStudyIcon: React.ReactNode = <FontAwesomeIcon icon={faFlask} />;
@@ -19,27 +23,44 @@ const UserProfileNavbar: React.FC<UserProfileNavbarProps> = () => {
   );
   const tutorialsIcon: React.ReactNode = <FontAwesomeIcon icon={faBookOpen} />;
 
+  const currentPath: string = `/user/${username}`;
+
   return (
     <nav className={styles['user-profile-navbar']}>
-      <div className={styles['user-nav-item']}>
+      <div
+        className={styles['user-nav-item']}
+        onClick={() => navigate(`${currentPath}/`)}
+      >
         <div className={styles['user-nav-icon']}>{latestIcon}</div>
         Latest
       </div>
-      <div className={styles['user-nav-item']}>
+      <div
+        className={styles['user-nav-item']}
+        onClick={() => navigate(`${currentPath}/projects`)}
+      >
         <div className={styles['user-nav-icon']}>{projectsIcon}</div>
         Projects
         <div className={styles['user-nav-number']}>0</div>
       </div>
-      <div className={styles['user-nav-item']}>
+      <div
+        className={styles['user-nav-item']}
+        onClick={() => navigate(`${currentPath}/case-studies`)}
+      >
         <div className={styles['user-nav-icon']}>{caseStudyIcon}</div>Case
         Studies
         <div className={styles['user-nav-number']}>0</div>
       </div>
-      <div className={styles['user-nav-item']}>
+      <div
+        className={styles['user-nav-item']}
+        onClick={() => navigate(`${currentPath}/tutorials`)}
+      >
         <div className={styles['user-nav-icon']}>{tutorialsIcon}</div>Tutorials
         <div className={styles['user-nav-number']}>0</div>
       </div>
-      <div className={styles['user-nav-item']}>
+      <div
+        className={styles['user-nav-item']}
+        onClick={() => navigate(`${currentPath}/collections`)}
+      >
         <div className={styles['user-nav-icon']}>{collectionsIcon}</div>
         Collections
         <div className={styles['user-nav-number']}>0</div>
