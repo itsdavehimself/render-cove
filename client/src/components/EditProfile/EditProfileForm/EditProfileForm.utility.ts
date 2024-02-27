40755343b33d82aa4384bb02417ea6ff2fabd857
David <23064280+itsdavehimself@users.noreply.github.com>
2024-02-27 08:06:20 -0600

    Move tag functions to own utility file

--- client/src/components/EditProfile/EditProfileForm/EditProfileForm.utility.ts
+++ client/src/components/EditProfile/EditProfileForm/EditProfileForm.utility.ts
@@ -1,58 +1,5 @@
 import imageCompression from 'browser-image-compression';
 
-const addTag = (
-  e: React.KeyboardEvent<HTMLInputElement>,
-  setTagList: React.Dispatch<React.SetStateAction<string[]>>,
-  tagList: string[],
-  setInputWidth: React.Dispatch<React.SetStateAction<number>>,
-): void => {
-  const inputValue: string = e.currentTarget.value.trim();
-
-  if (inputValue !== '') {
-    const tagValue: string = e.currentTarget.value.replace(/,/g, '').trim();
-
-    if (tagValue !== '') {
-      setTagList([...tagList, tagValue]);
-      e.currentTarget.value = '';
-      setInputWidth(1);
-    }
-  }
-};
-
-const removeTag = (
-  indexToRemove: number,
-  setTagList: React.Dispatch<React.SetStateAction<string[]>>,
-  tagList: string[],
-): void => {
-  setTagList(tagList.filter((_, index) => index !== indexToRemove));
-};
-
-const handleInputChange = (
-  inputRef: React.MutableRefObject<HTMLInputElement | null>,
-  setInputWidth: React.Dispatch<React.SetStateAction<number>>,
-): void => {
-  if (inputRef.current) {
-    setInputWidth(inputRef.current.value.length);
-  }
-};
-
-const handleInputClick = (
-  inputRef: React.MutableRefObject<HTMLInputElement | null>,
-): void => {
-  if (inputRef.current) {
-    inputRef.current.focus();
-  }
-};
-
-const preventEnterKeySubmission = (
-  e: React.KeyboardEvent<HTMLFormElement>,
-): void => {
-  const target = e.target as HTMLInputElement | HTMLTextAreaElement;
-  if (e.key === 'Enter' && !['TEXTAREA', 'BUTTON'].includes(target.tagName)) {
-    e.preventDefault();
-  }
-};
-
 const customFileValidation = (
   file: File,
   allowedFileTypes: string[],
@@ -110,12 +57,4 @@ const compressAndSetPreview = async (
   }
 };
 
-export {
-  addTag,
-  removeTag,
-  handleInputChange,
-  handleInputClick,
-  preventEnterKeySubmission,
-  customFileValidation,
-  compressAndSetPreview,
-};
+export { customFileValidation, compressAndSetPreview };
