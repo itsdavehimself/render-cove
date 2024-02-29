b9e49b4fb3bfc8cdc8301d1137cef4f3ad5b3dbf
David <23064280+itsdavehimself@users.noreply.github.com>
2024-02-29 13:05:54 -0600

    Extract and set metadata

--- client/src/components/CreateProjectForm/CreateProjectForm.utility.ts
+++ client/src/components/CreateProjectForm/CreateProjectForm.utility.ts
@@ -1,10 +1,75 @@
 import imageCompression from 'browser-image-compression';
+import exifr from 'exifr';
+import ImageData from '../../types/ImageData';
+const promptRegex = /^[^\r\n]*/;
+const negativePromptRegex = /(?<=Negative prompt: )[\s\S]*(?=\W\s\r?\nSteps)/;
+const stepsRegex = /(?<=Steps: )[\s\S]*(?=\W\sSampler)/;
+const samplerRegex = /(?<=Sampler: )[\s\S]*(?=\W\sCFG)/;
+const cfgScaleRegex = /(?<=CFG scale: )[\s\S]*(?=\W\sSeed)/;
+const seedRegex = /(?<=Seed: )[\s\S]*(?=\W\sSize)/;
+const modelRegex = /(?<=Model: )[\s\S]+?(?=\W\s(?:Denoising|Clip)|$)/;
 
-interface ImageData {
-  image: string | ArrayBuffer | null;
-  compressedImage: File;
-  caption: string;
-}
+const getImageMetadata = async (acceptedFiles: File[]) => {
+  const metadata = await Promise.all(
+    acceptedFiles.map(async (file) => {
+      try {
+        const rawMetadata = await exifr.parse(file);
+
+        if (rawMetadata && rawMetadata.parameters) {
+          const parameters = rawMetadata.parameters;
+
+          const promptArr = parameters.match(promptRegex);
+          const prompt = promptArr ? promptArr.join('').trim() : '';
+
+          const negativePromptArr = parameters.match(negativePromptRegex);
+          const negativePrompt = negativePromptArr
+            ? negativePromptArr.join('')
+            : '';
+
+          const stepsArr = parameters.match(stepsRegex);
+          const steps = stepsArr ? parseInt(stepsArr.join(''), 10) : 0;
+
+          const samplerArr = parameters.match(samplerRegex);
+          const sampler = samplerArr ? samplerArr.join('') : '';
+
+          const cfgScaleArr = parameters.match(cfgScaleRegex);
+          const cfgScale = cfgScaleArr ? parseInt(cfgScaleArr.join(''), 10) : 0;
+
+          const seedArr = parameters.match(seedRegex);
+          const seed = seedArr ? parseInt(seedArr.join(''), 10) : 0;
+
+          const modelArr = parameters.match(modelRegex);
+          const model = modelArr ? modelArr.join('') : '';
+
+          return {
+            prompt,
+            negativePrompt,
+            steps,
+            sampler,
+            cfgScale,
+            seed,
+            model,
+          };
+        } else {
+          return {
+            model: '',
+            prompt: '',
+            negativePrompt: '',
+            cfgScale: 0,
+            steps: 0,
+            sampler: '',
+            seed: 0,
+          };
+        }
+      } catch (error) {
+        console.error(`Failed to get metadata for file ${file.name}:`, error);
+        throw error;
+      }
+    }),
+  );
+
+  return metadata;
+};
 
 const compressAndSetPreviewMultiple = async (
   acceptedFiles: File[],
@@ -16,6 +81,8 @@ const compressAndSetPreviewMultiple = async (
   },
   setCompressedImages: React.Dispatch<React.SetStateAction<File[] | undefined>>,
 ) => {
+  const metadata = await getImageMetadata(acceptedFiles);
+
   try {
     const processFileAtIndex = async (index: number) => {
       const compressedFiles = await Promise.all(
@@ -39,12 +106,19 @@ const compressAndSetPreviewMultiple = async (
         const fileReader = new FileReader();
 
         fileReader.onload = function () {
-          setImageData((prevImageDatas) => [
-            ...prevImageDatas,
+          setImageData((prevImageData) => [
+            ...prevImageData,
             {
               image: fileReader.result,
               compressedImage: compressedFile,
               caption: '',
+              model: metadata[index].model,
+              prompt: metadata[index].prompt,
+              negativePrompt: metadata[index].negativePrompt,
+              cfgScale: metadata[index].cfgScale,
+              steps: metadata[index].steps,
+              sampler: metadata[index].sampler,
+              seed: metadata[index].seed,
             },
           ]);
           processFileAtIndex(index + 1);
