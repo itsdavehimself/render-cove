8e59f383f50a160514fb161912d522d0302a9527
David <23064280+itsdavehimself@users.noreply.github.com>
2024-02-29 13:05:04 -0600

    Factor image upload preview cards

--- client/src/components/MultiImageInput/MultiImageInput.tsx
+++ client/src/components/MultiImageInput/MultiImageInput.tsx
@@ -5,20 +5,12 @@ import {
   DropzoneInputProps,
 } from 'react-dropzone';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faImage, faXmark } from '@fortawesome/free-solid-svg-icons';
-
-interface ImageData {
-  image: string | ArrayBuffer | null;
-  compressedImage: File;
-  caption: string;
-}
+import { faImage } from '@fortawesome/free-solid-svg-icons';
 
 interface MultiImageInputProps {
   getRootProps: <T extends DropzoneRootProps>(props?: T | undefined) => T;
   getInputProps: <T extends DropzoneInputProps>(props?: T | undefined) => T;
   isDragActive: boolean;
-  imageData: ImageData[];
-  setImageData: React.Dispatch<React.SetStateAction<ImageData[]>>;
   fileRejections: FileRejection[];
   fileSizeLimit: string;
 }
@@ -27,27 +19,10 @@ const MultiImageInput: React.FC<MultiImageInputProps> = ({
   getRootProps,
   getInputProps,
   isDragActive,
-  imageData,
-  setImageData,
   fileRejections,
   fileSizeLimit,
 }) => {
-  const deleteIcon: React.ReactNode = <FontAwesomeIcon icon={faXmark} />;
   const imageIcon: React.ReactNode = <FontAwesomeIcon icon={faImage} />;
-
-  const handleDeleteImage = (indexToRemove: number): void => {
-    setImageData((prevImageData) =>
-      prevImageData.filter((_, index) => index !== indexToRemove),
-    );
-  };
-
-  const handleCaptionChange = (index: number, newCaption: string): void => {
-    setImageData((prevImageData) =>
-      prevImageData.map((data, i) =>
-        i === index ? { ...data, caption: newCaption } : data,
-      ),
-    );
-  };
 
   return (
     <div className={styles['image-upload-container']}>
@@ -82,44 +57,6 @@ const MultiImageInput: React.FC<MultiImageInputProps> = ({
           </div>
         </div>
       </div>
-      {imageData.length > 0 && (
-        <div className={styles['preview-cards-container']}>
-          {imageData.map((data, index) => (
-            <div className={styles['preview-card']} key={index}>
-              <div className={styles['image-container']}>
-                <img
-                  className={`${styles['image-preview']}`}
-                  src={
-                    typeof data.image === 'string'
-                      ? data.image
-                      : data.image instanceof ArrayBuffer
-                        ? 'data:image/jpeg;base64,' +
-                          Buffer.from(data.image).toString('base64')
-                        : ''
-                  }
-                />
-                <div className={styles['card-top-bar']}>
-                  <button
-                    type="button"
-                    className={styles['trash-button']}
-                    onClick={() => handleDeleteImage(index)}
-                  >
-                    {deleteIcon}
-                  </button>
-                </div>
-              </div>
-              <div className={styles['card-caption-container']}>
-                <textarea
-                  className={`${styles['form-input']} ${styles['textarea']}`}
-                  placeholder="Enter a brief description or caption"
-                  onChange={(e) => handleCaptionChange(index, e.target.value)}
-                  value={imageData[index].caption}
-                ></textarea>
-              </div>
-            </div>
-          ))}
-        </div>
-      )}
     </div>
   );
 };
