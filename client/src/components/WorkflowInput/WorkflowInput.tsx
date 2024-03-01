eeabfbadfb98ff27ea9583ac08a7d32e45afdfc7
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-01 13:48:04 -0600

    Add styles

--- client/src/components/WorkflowInput/WorkflowInput.tsx
+++ client/src/components/WorkflowInput/WorkflowInput.tsx
@@ -1,13 +1,21 @@
 import styles from './WorkflowInput.module.scss';
 import { useCurrentEditor } from '@tiptap/react';
+import Tiptap from '../Tiptap/Tiptap.tsx';
+import { useState } from 'react';
 
 interface WorkflowInputProps {}
 
 const WorkflowInput: React.FC<WorkflowInputProps> = () => {
   const { editor } = useCurrentEditor();
+  const [isEditorFocused, setIsEditorFocused] = useState<boolean>(false);
 
   return (
-    <div className={styles['workflow-container']}>
+    <div
+      className={`${styles['workflow-container']} ${isEditorFocused ? styles['editor-focused'] : ''}`}
+      onFocus={() => setIsEditorFocused(true)}
+      onBlur={() => setIsEditorFocused(false)}
+    >
+      <Tiptap />
       <pre>{editor && JSON.stringify(editor.getJSON(), null, 2)}</pre>
     </div>
   );
