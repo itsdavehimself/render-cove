2e1761933d82afc20f188bd3cf27c9c918f11b84
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-01 15:44:15 -0600

    Add optional icon

--- client/src/components/SaveSubmitButton/SaveSubmitButton.tsx
+++ client/src/components/SaveSubmitButton/SaveSubmitButton.tsx
@@ -1,12 +1,14 @@
 import styles from './SaveSubmitButton.module.scss';
 
 interface SaveSubmitButtonProps {
+  icon?: React.ReactNode;
   label: string;
   isLoading: boolean;
   color: string;
 }
 
 const SaveSubmitButton: React.FC<SaveSubmitButtonProps> = ({
+  icon,
   label,
   isLoading,
   color,
@@ -16,7 +18,14 @@ const SaveSubmitButton: React.FC<SaveSubmitButtonProps> = ({
       className={`${styles[`save-submit-button-${color}`]} ${isLoading ? styles['submitting'] : ''}`}
       disabled={isLoading}
     >
-      {isLoading ? <span className={styles['loader-spinner']}></span> : label}
+      {isLoading ? (
+        <span className={styles['loader-spinner']}></span>
+      ) : (
+        <div className={styles['button-label']}>
+          {icon && <span>{icon}</span>}
+          <span>{label}</span>
+        </div>
+      )}
     </button>
   );
 };
