f1f1d47a6f923ec3802897affe2d7d937ef5abbe
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-01 15:15:06 -0600

    Require color as prop

--- client/src/components/SaveSubmitButton/SaveSubmitButton.tsx
+++ client/src/components/SaveSubmitButton/SaveSubmitButton.tsx
@@ -3,15 +3,17 @@ import styles from './SaveSubmitButton.module.scss';
 interface SaveSubmitButtonProps {
   label: string;
   isLoading: boolean;
+  color: string;
 }
 
 const SaveSubmitButton: React.FC<SaveSubmitButtonProps> = ({
   label,
   isLoading,
+  color,
 }) => {
   return (
     <button
-      className={`${styles['save-submit-button']} ${isLoading ? styles['submitting'] : ''}`}
+      className={`${styles[`save-submit-button-${color}`]} ${isLoading ? styles['submitting'] : ''}`}
       disabled={isLoading}
     >
       {isLoading ? <span className={styles['loader-spinner']}></span> : label}
