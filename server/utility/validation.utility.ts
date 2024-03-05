f6650e3c313a9deb8af3e674c25d13f9fb29fbc0
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-05 08:29:34 -0600

    Rename file

---
+++ server/utility/validation.utility.ts
@@ -0,0 +1,52 @@
+import User from '../models/userModel.js';
+import { Request, Response } from 'express';
+
+const validateUsername = async (newUsername: string, res: Response) => {
+  const usernameExists = await User.findOne({ username: newUsername });
+
+  if (usernameExists && newUsername !== usernameExists.username) {
+    res
+      .status(400)
+      .json({ error: 'Username exists. Please try another username.' });
+    throw new Error('Username already exists. Please try another username.');
+  }
+};
+
+const checkEmptyProjectFields = (
+  title: string,
+  description: string,
+  projectImages: Express.Multer.File[] | undefined,
+  parsedWorkflow: object,
+  parsedSoftwareList: string[],
+  parsedTags: string[]
+): string[] => {
+  let emptyFields: string[] = [];
+
+  if (!title) {
+    emptyFields.push('title');
+  }
+
+  if (!description) {
+    emptyFields.push('description');
+  }
+
+  if (!projectImages) {
+    emptyFields.push('project images');
+  }
+
+  if (!parsedWorkflow) {
+    emptyFields.push('workflow');
+  }
+
+  if (parsedSoftwareList.length < 1) {
+    emptyFields.push('software');
+  }
+
+  if (parsedTags.length < 1) {
+    emptyFields.push('tags');
+  }
+
+  return emptyFields;
+};
+
+export { validateUsername, checkEmptyProjectFields };
