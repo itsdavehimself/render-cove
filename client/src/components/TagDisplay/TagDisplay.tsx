254a51ad325a132cd795aab4a513b315a863d3e3
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-06 14:07:43 -0600

    Create tag display component

---
+++ client/src/components/TagDisplay/TagDisplay.tsx
@@ -0,0 +1,23 @@
+import styles from './TagDisplay.module.scss';
+
+interface TagDisplayProps {
+  header: string;
+  tagList: string[] | undefined;
+}
+
+const TagDisplay: React.FC<TagDisplayProps> = ({ header, tagList }) => {
+  return (
+    <div className={styles['tag-container']}>
+      <h4 className={styles['tag-header']}>{header}</h4>
+      <div className={styles.tags}>
+        {tagList?.map((tag, index) => (
+          <div key={index} className={styles.tag}>
+            {tag}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TagDisplay;
