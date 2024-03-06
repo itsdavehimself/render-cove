6233fbe42c2fe1b37c1134749a5417468175dfa1
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-06 09:41:02 -0600

    Create card popout menu

--- client/src/components/ProjectCard/ProjectCard.tsx
+++ client/src/components/ProjectCard/ProjectCard.tsx
@@ -1,5 +1,13 @@
 import styles from './ProjectCard.module.scss';
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faEllipsisVertical,
+  faFlag,
+  faEye,
+} from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import PopOutMenu from '../PopOutMenu/PopOutMenu';
 
 interface ProjectCardProps {
   title: string;
@@ -17,13 +25,51 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   projectId,
 }) => {
   const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
   const handleProjectClick = (): void => {
     navigate(`/project/${projectId}`);
   };
 
+  const handleOptionClick = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    e.stopPropagation();
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const ellipsisIcon: React.ReactNode = (
+    <FontAwesomeIcon icon={faEllipsisVertical} />
+  );
+  const reportIcon: React.ReactNode = <FontAwesomeIcon icon={faFlag} />;
+  const viewIcon: React.ReactNode = <FontAwesomeIcon icon={faEye} />;
+
   return (
     <div className={styles['project-card']} onClick={handleProjectClick}>
+      {isMenuOpen && (
+        <div className={styles['popout-menu-container']}>
+          <PopOutMenu
+            buttons={[
+              {
+                icon: reportIcon,
+                label: 'Report Project',
+                onClick: () => console.log('project reported'),
+              },
+              {
+                icon: viewIcon,
+                label: 'View Project',
+                onClick: () => handleProjectClick,
+              },
+            ]}
+          />
+        </div>
+      )}
+      <div className={styles['card-options']}>
+        <button
+          className={styles['options-button']}
+          onClick={handleOptionClick}
+        >
+          {ellipsisIcon}
+        </button>
+      </div>
       <div className={styles.info}>
         <div className={styles['avatar-container']}>
           <img className={styles.avatar} src={avatarUrl}></img>
