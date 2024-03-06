98c61fb0954aaf14e677e8bc5cab2eb0744c2d1e
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-05 20:45:34 -0600

    Add card information and styles

--- client/src/components/ProjectCard/ProjectCard.tsx
+++ client/src/components/ProjectCard/ProjectCard.tsx
@@ -1,12 +1,39 @@
 import styles from './ProjectCard.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 interface ProjectCardProps {
+  title: string;
+  author: string;
   imageUrl: string;
+  avatarUrl: string;
+  projectId: string;
 }
 
-const ProjectCard: React.FC<ProjectCardProps> = ({ imageUrl }) => {
+const ProjectCard: React.FC<ProjectCardProps> = ({
+  title,
+  author,
+  imageUrl,
+  avatarUrl,
+  projectId,
+}) => {
+  const navigate = useNavigate();
+
+  const handleProjectClick = (): void => {
+    navigate(`/project/${projectId}`);
+  };
+
   return (
-    <div className={styles['project-card']}>
+    <div className={styles['project-card']} onClick={handleProjectClick}>
+      <div className={styles.info}>
+        <div className={styles['avatar-container']}>
+          <img className={styles.avatar} src={avatarUrl}></img>
+        </div>
+        <div className={styles['project-info']}>
+          <h4 className={styles.title}>{title}</h4>
+          <h5 className={styles.author}>{author}</h5>
+        </div>
+      </div>
+      <div className={styles['card-overlay']}></div>
       <img className={styles['project-image']} src={imageUrl}></img>
     </div>
   );
