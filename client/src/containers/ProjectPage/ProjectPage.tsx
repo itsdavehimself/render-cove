5288e6fce2c3712520a51b66245798756f4a7e06
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-06 15:03:59 -0600

    Fetch project and artist info

--- client/src/containers/ProjectPage/ProjectPage.tsx
+++ client/src/containers/ProjectPage/ProjectPage.tsx
@@ -1,10 +1,62 @@
+import ProjectPageSidebar from '../../components/ProjectPage/ProjectPageSidebar/ProjectPageSidebar';
 import styles from './ProjectPage.module.scss';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Project from '../../types/Project';
+import UserInfo from '../../types/UserInfo';
+
+const API_BASE_URL: string =
+  import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000/api';
 
 const ProjectPage: React.FC = () => {
-  const { id } = useParams();
+  const { projectId } = useParams();
+  const [projectInfo, setProjectInfo] = useState<Project | undefined>();
+  const [artistInfo, setArtistInfo] = useState<UserInfo | undefined>();
 
-  return <div>Project {id}</div>;
+  useEffect(() => {
+    const fetchProjectInfo = async (): Promise<void> => {
+      try {
+        const projectInfoResponse = await fetch(
+          `${API_BASE_URL}/projects/${projectId}`,
+          {
+            method: 'GET',
+          },
+        );
+
+        if (projectInfoResponse.ok) {
+          const projectInfoData = await projectInfoResponse.json();
+          setProjectInfo(projectInfoData);
+
+          const artistInfoResponse = await fetch(
+            `${API_BASE_URL}/users/${projectInfoData.author}`,
+            {
+              method: 'GET',
+            },
+          );
+
+          if (artistInfoResponse.ok) {
+            const artistInfoData = await artistInfoResponse.json();
+            setArtistInfo(artistInfoData);
+          } else {
+            console.error(artistInfoResponse.json());
+          }
+        } else {
+          console.error(projectInfoResponse.json());
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProjectInfo();
+  }, [projectId]);
+
+  return (
+    <div className={styles['project-container']}>
+      <div>Main Content</div>
+      <ProjectPageSidebar projectInfo={projectInfo} artistInfo={artistInfo} />
+    </div>
+  );
 };
 
 export default ProjectPage;
