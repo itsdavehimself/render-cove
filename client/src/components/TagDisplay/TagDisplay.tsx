0fae33a4f47321780a5e7bd1bf367fd57ca713dd
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-10 16:28:49 -0500

    Omit tag if empty string

--- client/src/components/TagDisplay/TagDisplay.tsx
+++ client/src/components/TagDisplay/TagDisplay.tsx
@@ -10,11 +10,14 @@ const TagDisplay: React.FC<TagDisplayProps> = ({ header, tagList }) => {
     <div className={styles['tag-container']}>
       <h4 className={styles['tag-header']}>{header}</h4>
       <div className={styles.tags}>
-        {tagList?.map((tag, index) => (
-          <div key={index} className={styles.tag}>
-            {tag}
-          </div>
-        ))}
+        {tagList?.map(
+          (tag, index) =>
+            tag !== '' && (
+              <div key={index} className={styles.tag}>
+                {tag}
+              </div>
+            ),
+        )}
       </div>
     </div>
   );
