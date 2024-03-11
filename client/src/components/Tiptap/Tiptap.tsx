15bab3213d522a61948185f11dd3c954d5d867ea
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-11 14:23:08 -0500

    Render existing info on editing page

--- client/src/components/Tiptap/Tiptap.tsx
+++ client/src/components/Tiptap/Tiptap.tsx
@@ -2,8 +2,19 @@ import './Tiptap.styles.scss';
 import TiptapMenu from './TiptapMenu/TiptapMenu';
 import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight';
 import { common, createLowlight } from 'lowlight';
-import { EditorProvider } from '@tiptap/react';
+import { EditorProvider, generateHTML } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import { useMemo } from 'react';
+import Bold from '@tiptap/extension-bold';
+import Italic from '@tiptap/extension-italic';
+import Strike from '@tiptap/extension-strike';
+import Document from '@tiptap/extension-document';
+import Paragraph from '@tiptap/extension-paragraph';
+import Text from '@tiptap/extension-text';
+import Heading from '@tiptap/extension-heading';
+import ListItem from '@tiptap/extension-list-item';
+import BulletList from '@tiptap/extension-bullet-list';
+import OrderedList from '@tiptap/extension-ordered-list';
 
 const lowlight = createLowlight(common);
 
@@ -16,18 +27,35 @@ const extensions = [
 
 interface TiptapProps {
   onUpdate: (content: object) => void;
-  currentContent?: object;
+  currentContent?: object | null;
 }
 
 const Tiptap: React.FC<TiptapProps> = ({ onUpdate, currentContent }) => {
+  const editorContent = useMemo(() => {
+    if (currentContent) {
+      return generateHTML(currentContent, [
+        Document,
+        Paragraph,
+        Text,
+        Bold,
+        Italic,
+        Strike,
+        Heading,
+        ListItem,
+        BulletList,
+        OrderedList,
+        CodeBlockLowlight,
+      ]);
+    } else {
+      return '<p>Write about your workflow here...</p>';
+    }
+  }, [currentContent]);
+
   return (
     <EditorProvider
+      key={currentContent ? 'editor_with_content' : 'editor_without_content'}
       extensions={extensions}
-      content={
-        currentContent
-          ? currentContent
-          : '<p>Write about your workflow here...</p>'
-      }
+      content={editorContent}
       onUpdate={({ editor }) => {
         onUpdate(editor.getJSON());
       }}
