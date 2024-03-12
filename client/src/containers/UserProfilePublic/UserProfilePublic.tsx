58fc1d88402a8f5899b369c94849f0eb8173edfb
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-12 13:29:08 -0500

    Wrap main content in all projects context provider

--- client/src/containers/UserProfilePublic/UserProfilePublic.tsx
+++ client/src/containers/UserProfilePublic/UserProfilePublic.tsx
@@ -2,13 +2,16 @@ import styles from './UserProfilePublic.module.scss';
 import UserProfileSidebar from '../../components/UserProfile/UserProfileSidebar/UserProfileSidebar';
 import { UserInfoContextProvider } from '../../context/UserInfoContext';
 import UserProfileMainContent from '../../components/UserProfile/UserProfileMainContent/UserProfileMainContent';
+import { AllProjectsContextProvider } from '../../context/AllProjectsContext';
 
 const UserProfilePublic: React.FC = () => {
   return (
     <UserInfoContextProvider>
       <div className={styles['public-profile-container']}>
         <main className={styles.main}>
-          <UserProfileMainContent />
+          <AllProjectsContextProvider>
+            <UserProfileMainContent />
+          </AllProjectsContextProvider>
         </main>
         <aside className={styles.sidebar}>
           <UserProfileSidebar />
