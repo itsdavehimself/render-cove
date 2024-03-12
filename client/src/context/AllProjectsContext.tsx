7200bd818f73b4bd6c107879139621231bdca018
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-12 13:23:30 -0500

    Create all projects context provider

---
+++ client/src/context/AllProjectsContext.tsx
@@ -0,0 +1,61 @@
+import {
+  ReactNode,
+  createContext,
+  useReducer,
+  Dispatch,
+  useEffect,
+} from 'react';
+import Project from '../types/Project';
+import { useParams } from 'react-router-dom';
+import { AllProjectsAction } from '../types/AllProjectsActionTypes';
+import { allProjectsReducer } from '../reducers/allProjectsReducer';
+
+interface AllProjectsContextType {
+  dispatshAllProjects: Dispatch<AllProjectsAction>;
+  allProjects: Project[];
+}
+
+const API_BASE_URL: string =
+  import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000/api';
+
+const AllProjectsContext = createContext<AllProjectsContextType | null>(null);
+
+const AllProjectsContextProvider = ({ children }: { children: ReactNode }) => {
+  const { username } = useParams();
+  const [state, dispatchAllProjects] = useReducer(allProjectsReducer, {
+    projects: null,
+  });
+
+  useEffect(() => {
+    if (username) {
+      const fetchUserProjects = async (): Promise<void> => {
+        try {
+          const projectsResponse = await fetch(
+            `${API_BASE_URL}/projects/user/${username}`,
+            { method: 'GET' },
+          );
+
+          if (projectsResponse.ok) {
+            const allProjects = await projectsResponse.json();
+            dispatchAllProjects({
+              type: 'GET_ALL_PROJECTS',
+              payload: allProjects,
+            });
+          } else {
+            console.error(projectsResponse.json());
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      fetchUserProjects();
+    }
+  }, [username]);
+  return (
+    <AllProjectsContext.Provider value={{ ...state, dispatchAllProjects }}>
+      {children}
+    </AllProjectsContext.Provider>
+  );
+};
+
+export { AllProjectsContext, AllProjectsContextProvider };
