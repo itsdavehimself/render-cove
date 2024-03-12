322130b49b1319911643445090b42dc92f990b72
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-12 14:20:42 -0500

    Render conditionally if user & project published

--- client/src/containers/ProjectPage/ProjectPage.tsx
+++ client/src/containers/ProjectPage/ProjectPage.tsx
@@ -4,13 +4,17 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ProjectPageMainContent from '../../components/ProjectPage/ProjectPageMainContent/ProjectPageMainContent';
 import { GenerationData } from '../../types/Project';
-import { ProjectContextProvider } from '../../context/ProjectContext';
+import { useProjectContext } from '../../hooks/useProjectContext';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 const API_BASE_URL: string =
   import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000/api';
 
 const ProjectPage: React.FC = () => {
   const { projectId } = useParams();
+  const { project, artist } = useProjectContext();
+  const { user } = useAuthContext();
+
   const [generationData, setGenerationData] = useState<GenerationData>({
     prompt: '',
     negativePrompt: '',
@@ -45,22 +49,26 @@ const ProjectPage: React.FC = () => {
   }, [projectId]);
 
   return (
-    <ProjectContextProvider>
-      <div className={styles['project-container']}>
-        <main className={styles.main}>
-          <ProjectPageMainContent
-            generationData={generationData}
-            setGenerationData={setGenerationData}
-          />
-        </main>
-        <aside className={styles.sidebar}>
-          <ProjectPageSidebar
-            generationData={generationData}
-            API_BASE_URL={API_BASE_URL}
-          />
-        </aside>
-      </div>
-    </ProjectContextProvider>
+    <>
+      {!project?.published && user?.userId !== artist?._id ? (
+        <div>Project is not published</div>
+      ) : (
+        <div className={styles['project-container']}>
+          <main className={styles.main}>
+            <ProjectPageMainContent
+              generationData={generationData}
+              setGenerationData={setGenerationData}
+            />
+          </main>
+          <aside className={styles.sidebar}>
+            <ProjectPageSidebar
+              generationData={generationData}
+              API_BASE_URL={API_BASE_URL}
+            />
+          </aside>
+        </div>
+      )}
+    </>
   );
 };
 
