e26e9d638a267b2510285fbb79d734164ad85029
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-12 13:29:47 -0500

    Render all projects conditionally

--- client/src/components/UserProfile/UserProfileProjects/UserProfileProjects.tsx
+++ client/src/components/UserProfile/UserProfileProjects/UserProfileProjects.tsx
@@ -1,50 +1,22 @@
 import { useUserInfoContext } from '../../../hooks/useUserInfoContext';
 import styles from './UserProfileProjects.module.scss';
-import { useEffect, useState } from 'react';
-import Project from '../../../types/Project';
 import ProjectCard from '../../ProjectCard/ProjectCard';
-
-const API_BASE_URL: string =
-  import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000/api';
+import { useAuthContext } from '../../../hooks/useAuthContext';
+import { useAllProjectsContext } from '../../../hooks/useAllProjectsContext';
 
 const UserProfileProjects: React.FC = () => {
+  const { user } = useAuthContext();
   const { userInfo } = useUserInfoContext();
-  const [projects, setProjects] = useState<Project[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const { allProjects } = useAllProjectsContext();
 
-  useEffect(() => {
-    if (userInfo) {
-      const fetchUserProjects = async (): Promise<void> => {
-        try {
-          const projectsResponse = await fetch(
-            `${API_BASE_URL}/projects/user/${userInfo._id}`,
-            { method: 'GET' },
-          );
-
-          if (projectsResponse.ok) {
-            const allProjects = await projectsResponse.json();
-            setProjects(allProjects);
-            setIsLoading(false);
-          } else {
-            console.error(projectsResponse.json());
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      };
-      fetchUserProjects();
-    }
-  }, [userInfo]);
   return (
     <>
       <section className={styles['user-profile-projects']}>
-        {isLoading ? (
-          <div>Loading...</div>
-        ) : (
+        {allProjects && (
           <>
-            {projects && (
+            {allProjects.map((project) => (
               <>
-                {projects.map((project) => (
+                {user.userId === userInfo._id ? (
                   <ProjectCard
                     title={project.title}
                     author={userInfo.username}
@@ -53,9 +25,22 @@ const UserProfileProjects: React.FC = () => {
                     projectId={project._id}
                     key={project._id}
                   />
-                ))}
+                ) : (
+                  <>
+                    {project.published === true && (
+                      <ProjectCard
+                        title={project.title}
+                        author={userInfo.username}
+                        imageUrl={project.images[0].url}
+                        avatarUrl={userInfo.avatarUrl}
+                        projectId={project._id}
+                        key={project._id}
+                      />
+                    )}
+                  </>
+                )}
               </>
-            )}
+            ))}
           </>
         )}
       </section>
