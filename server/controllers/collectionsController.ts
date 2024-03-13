82d97b26664940aa6e5f31c1468225ab7d145019
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-13 11:46:38 -0500

    Get all liked projects

---
+++ server/controllers/collectionsController.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+import { Types } from 'mongoose';
+import { UserDocument } from '../types/UserInterfaces.js';
+import ProjectDocument from '../types/ProjectDocument.js';
+import Project from '../models/projectModel.js';
+import User from '../models/userModel.js';
+
+interface AuthRequest extends Request {
+  user?: { _id: string };
+}
+
+const getAllUserLikes = async (req: AuthRequest, res: Response) => {
+  const userId = req.user?._id;
+
+  try {
+    const userWithPopulatedLikes = await User.findById(userId)
+      .populate({
+        path: 'likes.projectId',
+        select: 'title images _id',
+        match: { published: true },
+        populate: {
+          path: 'author',
+          select: 'avatarUrl username',
+        },
+      })
+      .exec();
+
+    const usersLikes = userWithPopulatedLikes?.likes;
+
+    if (usersLikes) {
+      res.status(200).json(usersLikes);
+    } else {
+      res.status(404).json({ message: 'No likes found for the user' });
+    }
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export { getAllUserLikes };
