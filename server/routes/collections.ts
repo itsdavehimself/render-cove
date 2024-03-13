01f387a8bd026af2608458df84ac5bbae51232ff
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-13 16:14:55 -0500

    Add create collection route

--- server/routes/collections.ts
+++ server/routes/collections.ts
@@ -1,9 +1,14 @@
 import express, { Router } from 'express';
-import { getAllUserLikes } from '../controllers/collectionsController.js';
+import {
+  getAllUserLikes,
+  createCollection,
+} from '../controllers/collectionsController.js';
 import requireAuth from '../middleware/requireAuth.js';
 
 const collectionsRouter: Router = express.Router();
 
 collectionsRouter.get('/likes', requireAuth, getAllUserLikes);
+
+collectionsRouter.post('/', requireAuth, createCollection);
 
 export default collectionsRouter;
