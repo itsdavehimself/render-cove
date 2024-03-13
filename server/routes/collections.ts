13fb9d9ff5c3ae752b5955a4384f44c178efdfae
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-13 18:25:50 -0500

    Create get collections route

--- server/routes/collections.ts
+++ server/routes/collections.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import {
   getAllUserLikes,
   createCollection,
+  getCollections,
 } from '../controllers/collectionsController.js';
 import requireAuth from '../middleware/requireAuth.js';
 
@@ -10,5 +11,7 @@ const collectionsRouter: Router = express.Router();
 collectionsRouter.get('/likes', requireAuth, getAllUserLikes);
 
 collectionsRouter.post('/', requireAuth, createCollection);
+
+collectionsRouter.get('/:userId', requireAuth, getCollections);
 
 export default collectionsRouter;
