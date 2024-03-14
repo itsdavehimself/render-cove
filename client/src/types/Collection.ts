1fd6e3363067d569c33a0befc3a9125617bd04f1
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-14 13:35:45 -0500

    Redefine projects array type

--- client/src/types/Collection.ts
+++ client/src/types/Collection.ts
@@ -1,7 +1,17 @@
+import UserInfo from './UserInfo';
+import { Image } from './Project';
+
+interface ProjectInCollection {
+  title: string;
+  author: UserInfo;
+  images: Image[];
+  _id: string;
+}
+
 interface Collection {
   title: string;
   creator: string;
-  projects: string[];
+  projects: ProjectInCollection[];
   createdAt: Date;
   private: boolean;
   _id: string;
