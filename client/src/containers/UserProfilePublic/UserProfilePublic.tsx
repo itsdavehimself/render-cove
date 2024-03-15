15ff2d936d107fbb6859f2d8be2e259774f6663f
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-15 15:40:31 -0500

    Wrap profile component inside collections provider

--- client/src/containers/UserProfilePublic/UserProfilePublic.tsx
+++ client/src/containers/UserProfilePublic/UserProfilePublic.tsx
@@ -3,15 +3,18 @@ import UserProfileSidebar from '../../components/UserProfile/UserProfileSidebar/
 import { UserInfoContextProvider } from '../../context/UserInfoContext';
 import UserProfileMainContent from '../../components/UserProfile/UserProfileMainContent/UserProfileMainContent';
 import { AllProjectsContextProvider } from '../../context/AllProjectsContext';
+import { CollectionsContextProvider } from '../../context/CollectionsContext';
 
 const UserProfilePublic: React.FC = () => {
   return (
     <UserInfoContextProvider>
       <div className={styles['public-profile-container']}>
         <main className={styles.main}>
-          <AllProjectsContextProvider>
-            <UserProfileMainContent />
-          </AllProjectsContextProvider>
+          <CollectionsContextProvider>
+            <AllProjectsContextProvider>
+              <UserProfileMainContent />
+            </AllProjectsContextProvider>
+          </CollectionsContextProvider>
         </main>
         <aside className={styles.sidebar}>
           <UserProfileSidebar />
