10ac5da07f7705d15683da55083384e0b16eab61
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-14 19:56:23 -0500

    Create delete collection route

--- server/routes/collections.ts
+++ server/routes/collections.ts
@@ -4,6 +4,7 @@ import {
   createCollection,
   getCollections,
   toggleInCollection,
+  deleteCollection,
 } from '../controllers/collectionsController.js';
 import requireAuth from '../middleware/requireAuth.js';
 
@@ -16,5 +17,7 @@ collectionsRouter.post('/', requireAuth, createCollection);
 collectionsRouter.get('/:userId', getCollections);
 
 collectionsRouter.patch('/:collectionId', requireAuth, toggleInCollection);
+
+collectionsRouter.delete('/:collectionId', requireAuth, deleteCollection);
 
 export default collectionsRouter;
