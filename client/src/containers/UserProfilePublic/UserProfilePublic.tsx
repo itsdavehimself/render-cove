a20afc805dfcdec3fd281a4d12483ef8775deb15
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-15 20:36:31 -0500

    Display modal when followers/following is clicked

--- client/src/containers/UserProfilePublic/UserProfilePublic.tsx
+++ client/src/containers/UserProfilePublic/UserProfilePublic.tsx
@@ -4,22 +4,31 @@ import { UserInfoContextProvider } from '../../context/UserInfoContext';
 import UserProfileMainContent from '../../components/UserProfile/UserProfileMainContent/UserProfileMainContent';
 import { AllProjectsContextProvider } from '../../context/AllProjectsContext';
 import { CollectionsContextProvider } from '../../context/CollectionsContext';
+import { useState } from 'react';
+import FollowerModal from '../../components/FollowerModal/FollowerModal';
 
 const UserProfilePublic: React.FC = () => {
+  const [openModal, setOpenModal] = useState<string>('none');
+
   return (
     <UserInfoContextProvider>
-      <div className={styles['public-profile-container']}>
-        <main className={styles.main}>
-          <CollectionsContextProvider>
-            <AllProjectsContextProvider>
-              <UserProfileMainContent />
-            </AllProjectsContextProvider>
-          </CollectionsContextProvider>
-        </main>
-        <aside className={styles.sidebar}>
-          <UserProfileSidebar />
-        </aside>
-      </div>
+      <>
+        {openModal !== 'none' && (
+          <FollowerModal openModal={openModal} setOpenModal={setOpenModal} />
+        )}
+        <div className={styles['public-profile-container']}>
+          <main className={styles.main}>
+            <CollectionsContextProvider>
+              <AllProjectsContextProvider>
+                <UserProfileMainContent />
+              </AllProjectsContextProvider>
+            </CollectionsContextProvider>
+          </main>
+          <aside className={styles.sidebar}>
+            <UserProfileSidebar setOpenModal={setOpenModal} />
+          </aside>
+        </div>
+      </>
     </UserInfoContextProvider>
   );
 };
