2788bc93391709e06c23d1ee377d0e3286f2acee
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-15 22:54:54 -0500

    Wrap user details in button for navigation

--- client/src/components/FollowerUser/FollowerUser.tsx
+++ client/src/components/FollowerUser/FollowerUser.tsx
@@ -1,29 +1,39 @@
 import styles from './FollowerUser.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 interface FollowerUserProps {
   username: string;
   displayName: string;
   avatarUrl: string;
-  _id: string;
+  setOpenModal: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const FollowerUser: React.FC<FollowerUserProps> = ({
   username,
   displayName,
   avatarUrl,
-  _id,
+  setOpenModal,
 }) => {
+  const navigate = useNavigate();
+
+  const handleClickUser = () => {
+    navigate(`/user/${username}`);
+    setOpenModal('none');
+  };
+
   return (
     <div className={styles['follower-card']}>
-      <div className={styles['follower-details']}>
-        <div className={styles['avatar-container']}>
-          <img src={avatarUrl}></img>
+      <button className={styles['follower-button']} onClick={handleClickUser}>
+        <div className={styles['follower-details']}>
+          <div className={styles['avatar-container']}>
+            <img src={avatarUrl}></img>
+          </div>
+          <div className={styles['follower-names']}>
+            <p className={styles['display-name']}>{displayName}</p>
+            <p className={styles.username}>{username}</p>
+          </div>
         </div>
-        <div className={styles['follower-names']}>
-          <p className={styles['display-name']}>{displayName}</p>
-          <p className={styles.username}>{username}</p>
-        </div>
-      </div>
+      </button>
       <button>Follow</button>
     </div>
   );
