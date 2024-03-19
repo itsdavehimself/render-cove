c7f48d1d96af6240efa2908b9c746d7a3ca36be6
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-19 14:04:10 -0500

    Create notification model

--- server/models/notificationModel.ts
+++ server/models/notificationModel.ts
@@ -5,6 +5,7 @@ const notificationSchema = new Schema<NotificationDocument>({
   recipient: { type: Schema.Types.ObjectId, ref: 'User' },
   sender: { type: Schema.Types.ObjectId, ref: 'User' },
   type: { type: String },
+  post: { type: Schema.Types.ObjectId, ref: 'Project' },
   createdAt: { type: Date, default: Date.now },
   read: { type: Boolean, default: false },
 });
