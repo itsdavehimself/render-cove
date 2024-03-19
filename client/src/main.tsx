cf2450be42ad04319fa02f8b16fa9ccd0b51dc3d
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-19 08:47:12 -0500

    Wrap app in notification context

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx';
 import './reset.module.scss';
 import './index.module.scss';
 import { AuthContextProvider } from './context/AuthContext.tsx';
+import { NotificationContextProvider } from './context/NotificationsContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <NotificationContextProvider>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </NotificationContextProvider>
   </React.StrictMode>,
 );
