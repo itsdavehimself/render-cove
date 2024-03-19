d69d1a5f77abcb24daebefad138903f016d01260
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-19 14:01:42 -0500

    Create mark as read utility function

---
+++ client/src/utility/Notifications.utility.ts
@@ -0,0 +1,28 @@
+import { UserType } from '../context/AuthContext';
+
+const API_BASE_URL: string =
+  import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000/api';
+
+const markAsRead = async (
+  user: UserType,
+  fetchNotifications: () => Promise<void>,
+): Promise<void> => {
+  const readResponse = await fetch(`${API_BASE_URL}/notifications/markAsRead`, {
+    method: 'PATCH',
+    headers: {
+      Authorization: `Bearer ${user.token}`,
+    },
+  });
+
+  const readJson = await readResponse.json();
+
+  if (!readResponse.ok) {
+    console.error(readJson.error);
+  }
+
+  if (readResponse.ok) {
+    fetchNotifications();
+  }
+};
+
+export { markAsRead };
