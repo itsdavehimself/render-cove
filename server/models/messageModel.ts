a36f46c1a7bf271876a081271c48e689df8bf21e
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-20 20:12:51 -0500

    Use timestamps instead of createdAt property

--- server/models/messageModel.ts
+++ server/models/messageModel.ts
@@ -1,12 +1,14 @@
 import mongoose, { Schema } from 'mongoose';
 import MessageDocument from '../types/MessageDocument';
 
-const messageSchema = new Schema<MessageDocument>({
-  recipient: { type: Schema.Types.ObjectId, ref: 'User' },
-  sender: { type: Schema.Types.ObjectId, ref: 'User' },
-  content: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  read: { type: Boolean, default: false },
-});
+const messageSchema = new Schema<MessageDocument>(
+  {
+    recipient: { type: Schema.Types.ObjectId, ref: 'User' },
+    sender: { type: Schema.Types.ObjectId, ref: 'User' },
+    content: { type: String, required: true },
+    read: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model<MessageDocument>('Message', messageSchema);
