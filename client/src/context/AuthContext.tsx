dadaec69fe4354ceb2c2e3c9fff5c1e60734e4ca
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-21 19:30:32 -0500

    Delete messages array definition

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -41,17 +41,6 @@ export interface UserType {
   followers: string[];
   likes: UserLike[];
   _id: string;
-  messages: [
-    {
-      withUser: {
-        avatarUrl: string;
-        displayName: string;
-        _id: string;
-      };
-      unreadCount: number;
-      _id: string;
-    },
-  ];
 }
 
 interface AuthContextType {
