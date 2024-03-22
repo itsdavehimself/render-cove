7a14749a8a540c77b840a60790650eab86b3cafc
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-21 19:28:50 -0500

    Wrap app in conversation context

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,13 +5,16 @@ import './reset.module.scss';
 import './index.module.scss';
 import { AuthContextProvider } from './context/AuthContext.tsx';
 import { NotificationContextProvider } from './context/NotificationsContext.tsx';
+import { ConversationContextProvider } from './context/ConversationsContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <NotificationContextProvider>
-        <App />
-      </NotificationContextProvider>
+      <ConversationContextProvider>
+        <NotificationContextProvider>
+          <App />
+        </NotificationContextProvider>
+      </ConversationContextProvider>
     </AuthContextProvider>
   </React.StrictMode>,
 );
