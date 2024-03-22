eee5e2b546b68bd98836865370d0e38fc2ad824a
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-21 19:32:13 -0500

    Create message api routes

--- server/routes/messages.ts
+++ server/routes/messages.ts
@@ -1,10 +1,19 @@
 import express, { Router } from 'express';
 import requireAuth from '../middleware/requireAuth.js';
-import { getMessages, sendMessage } from '../controllers/messagesController.js';
+import {
+  getMessages,
+  sendMessage,
+  getConversations,
+  markAsRead,
+} from '../controllers/messagesController.js';
 
 const messagesRouter: Router = express.Router();
 
+messagesRouter.get('/conversations', requireAuth, getConversations);
+
 messagesRouter.get('/:otherUserId', requireAuth, getMessages);
+
+messagesRouter.patch('/:otherUserId', requireAuth, markAsRead);
 
 messagesRouter.post('/:otherUserId', requireAuth, sendMessage);
 
