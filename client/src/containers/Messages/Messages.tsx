71909b7b20f601e9315120121fe240e1740af03f
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-23 10:51:51 -0500

    Wrap in UserInfoContextProvider

--- client/src/containers/Messages/Messages.tsx
+++ client/src/containers/Messages/Messages.tsx
@@ -1,20 +1,25 @@
 import AllMessages from '../../components/Messages/AllMessages/AllMessages';
 import { ThreadIndexProvider } from '../../context/ThreadIndexContext';
+import { UserInfoContextProvider } from '../../context/UserInfoContext';
 import styles from './Messages.module.scss';
 import { Outlet } from 'react-router-dom';
 
 const Messages: React.FC = () => {
   return (
     <ThreadIndexProvider>
-      <div className={styles['messages-container']}>
-        <aside className={styles['all-messages']}>
-          <AllMessages />
-        </aside>
-        <div className={styles['right-null']}>{/*Empty div for styling*/}</div>
-        <main className={styles.thread}>
-          <Outlet />
-        </main>
-      </div>
+      <UserInfoContextProvider>
+        <div className={styles['messages-container']}>
+          <aside className={styles['all-messages']}>
+            <AllMessages />
+          </aside>
+          <div className={styles['right-null']}>
+            {/*Empty div for styling*/}
+          </div>
+          <main className={styles.thread}>
+            <Outlet />
+          </main>
+        </div>
+      </UserInfoContextProvider>
     </ThreadIndexProvider>
   );
 };
