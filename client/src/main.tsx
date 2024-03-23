c779072acbd71c4c37afef263ce5eeadd7201828
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-23 08:51:40 -0500

    Move all context providers to main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,13 +5,25 @@ import './reset.module.scss';
 import './index.module.scss';
 import { AuthContextProvider } from './context/AuthContext.tsx';
 import { NotificationContextProvider } from './context/NotificationsContext.tsx';
+import SocketContextProvider from './context/SocketContext.tsx';
+import { ConversationContextProvider } from './context/ConversationsContext.tsx';
+import { io } from 'socket.io-client';
+
+const API_SOCKET_URL: string =
+  import.meta.env.VITE_SOCKET_URL || 'ws://localhost:4000';
+
+const socket = io(API_SOCKET_URL);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <NotificationContextProvider>
-        <App />
-      </NotificationContextProvider>
+      <SocketContextProvider socket={socket}>
+        <ConversationContextProvider>
+          <NotificationContextProvider>
+            <App />
+          </NotificationContextProvider>
+        </ConversationContextProvider>
+      </SocketContextProvider>
     </AuthContextProvider>
   </React.StrictMode>,
 );
