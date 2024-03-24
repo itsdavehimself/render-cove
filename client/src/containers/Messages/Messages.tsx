10503401f9c14a58828904e8e8a947dc02d7b0ea
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-24 17:01:53 -0500

    Remove context provider

--- client/src/containers/Messages/Messages.tsx
+++ client/src/containers/Messages/Messages.tsx
@@ -1,21 +1,18 @@
 import AllMessages from '../../components/Messages/AllMessages/AllMessages';
-import { UserInfoContextProvider } from '../../context/UserInfoContext';
 import styles from './Messages.module.scss';
 import { Outlet } from 'react-router-dom';
 
 const Messages: React.FC = () => {
   return (
-    <UserInfoContextProvider>
-      <div className={styles['messages-container']}>
-        <aside className={styles['all-messages']}>
-          <AllMessages />
-        </aside>
-        <div className={styles['right-null']}>{/*Empty div for styling*/}</div>
-        <main className={styles.thread}>
-          <Outlet />
-        </main>
-      </div>
-    </UserInfoContextProvider>
+    <div className={styles['messages-container']}>
+      <aside className={styles['all-messages']}>
+        <AllMessages />
+      </aside>
+      <div className={styles['right-null']}>{/*Empty div for styling*/}</div>
+      <main className={styles.thread}>
+        <Outlet />
+      </main>
+    </div>
   );
 };
 
