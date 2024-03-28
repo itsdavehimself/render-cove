a34f2c77729ca2e7008da52b8644e53d0dd4c7e5
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-28 16:33:43 -0500

    Create explore page

--- client/src/containers/Explore/Explore.tsx
+++ client/src/containers/Explore/Explore.tsx
@@ -1,9 +1,40 @@
+import TagBar from '../../components/TagBar/TagBar';
 import styles from './Explore.module.scss';
+import { useEffect, useState } from 'react';
+import Tag from '../../types/Tag';
+import { Outlet } from 'react-router-dom';
+
+const API_BASE_URL: string =
+  import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000/api';
 
 const Explore: React.FC = () => {
+  const [tags, setTags] = useState<Tag[]>([]);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchTags = async () => {
+      const tagsResponse = await fetch(`${API_BASE_URL}/tags/`, {
+        method: 'GET',
+      });
+
+      const tagsJson = await tagsResponse.json();
+
+      if (!tagsResponse.ok) {
+        setError(tagsJson.error);
+      }
+
+      if (tagsResponse.ok) {
+        setTags(tagsJson);
+      }
+    };
+
+    fetchTags();
+  }, []);
+
   return (
-    <main>
-      <header>Explore</header>
+    <main className={styles['explore-page']}>
+      <TagBar tags={tags} />
+      <Outlet />
     </main>
   );
 };
