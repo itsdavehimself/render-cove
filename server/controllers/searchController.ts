d494b451334bf0ee4f97f255e2bb994091f1fd74
David <23064280+itsdavehimself@users.noreply.github.com>
2024-03-30 11:22:13 -0500

    Create sort and filter logic

--- server/controllers/searchController.ts
+++ server/controllers/searchController.ts
@@ -1,16 +1,107 @@
 import { Request, Response } from 'express';
 import Project from '../models/projectModel.js';
+import User from '../models/userModel.js';
 
 const searchProjects = async (req: Request, res: Response) => {
   try {
     const query = req.query.query as string;
+    const sort = req.query.sort as string;
+    const filter = req.query.filter as string;
 
-    const results = await Project.find(
-      { $text: { $search: query } },
-      { score: { $meta: 'textScore' } }
-    ).sort({ score: { $meta: 'textScore' } });
+    let results;
 
-    console.log(results);
+    if (filter === 'users') {
+      if (sort === 'followers') {
+        results = await User.find({ $text: { $search: query } })
+          .select(
+            'displayName username avatarUrl tagline _id projects followers'
+          )
+          .sort({ followers: -1 })
+          .populate({
+            path: 'projects',
+            select: 'title',
+            populate: {
+              path: 'images',
+              select: 'url',
+            },
+          });
+      } else if (sort === 'projects') {
+        results = await User.find({ $text: { $search: query } })
+          .select(
+            'displayName username avatarUrl tagline _id projects followers'
+          )
+          .sort({ projects: -1 })
+          .populate({
+            path: 'projects',
+            select: 'title',
+            populate: {
+              path: 'images',
+              select: 'url',
+            },
+          });
+      } else if (sort === 'recent') {
+        results = await User.find({ $text: { $search: query } })
+          .select(
+            'displayName username avatarUrl tagline _id projects followers'
+          )
+          .sort({ createdAt: -1 })
+          .populate({
+            path: 'projects',
+            select: 'title',
+            populate: {
+              path: 'images',
+              select: 'url',
+            },
+          });
+      } else {
+        results = await User.find({ $text: { $search: query } })
+          .select(
+            'displayName username avatarUrl tagline _id projects followers'
+          )
+          .sort({ score: { $meta: 'textScore' } })
+          .populate({
+            path: 'projects',
+            select: 'title',
+            populate: {
+              path: 'images',
+              select: 'url',
+            },
+          });
+      }
+
+      res.status(200).json(results);
+      return;
+    }
+
+    if (sort === 'likes') {
+      results = await Project.find({ $text: { $search: query } })
+        .sort({ likes: -1 })
+        .populate({
+          path: 'author',
+          select: 'username displayName _id avatarUrl',
+        });
+    } else if (sort === 'views') {
+      results = await Project.find({ $text: { $search: query } })
+        .sort({ views: -1 })
+        .populate({
+          path: 'author',
+          select: 'username displayName _id avatarUrl',
+        });
+    } else if (sort === 'latest') {
+      results = await Project.find({ $text: { $search: query } })
+        .sort({ createdAt: -1 })
+        .populate({
+          path: 'author',
+          select: 'username displayName _id avatarUrl',
+        });
+    } else {
+      results = await Project.find({ $text: { $search: query } })
+        .sort({ score: { $meta: 'textScore' } })
+        .populate({
+          path: 'author',
+          select: 'username displayName _id avatarUrl',
+        });
+    }
 
     res.status(200).json(results);
   } catch (error) {
