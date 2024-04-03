8208b8b44f48abdf8c3742a3d28b12ab767db2f8
David <23064280+itsdavehimself@users.noreply.github.com>
2024-04-03 10:02:09 -0500

    Define state & action type

--- client/src/reducers/allProjectsReducer.ts
+++ client/src/reducers/allProjectsReducer.ts
@@ -1,4 +1,14 @@
-export const allProjectsReducer = (state, action) => {
+import { AllProjectsAction } from '../types/AllProjectsActionTypes';
+import Project from '../types/Project';
+
+interface AllProjectsState {
+  allProjects: Project[];
+}
+
+export const allProjectsReducer = (
+  state: AllProjectsState,
+  action: AllProjectsAction,
+) => {
   switch (action.type) {
     case 'GET_ALL_PROJECTS':
       return { allProjects: action.payload };
