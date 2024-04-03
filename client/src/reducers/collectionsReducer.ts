73d2de5b19bc3a16c4d2467d1fd5ae0290f2ed00
David <23064280+itsdavehimself@users.noreply.github.com>
2024-04-02 20:52:54 -0500

    Define state & action

--- client/src/reducers/collectionsReducer.ts
+++ client/src/reducers/collectionsReducer.ts
@@ -1,4 +1,14 @@
-export const collectionsReducer = (state, action) => {
+import { CollectionsAction } from '../types/CollectionsActionTypes';
+import Collection from '../types/Collection';
+
+interface CollectionsState {
+  collections: Collection[];
+}
+
+export const collectionsReducer = (
+  state: CollectionsState,
+  action: CollectionsAction,
+): CollectionsState => {
   switch (action.type) {
     case 'GET_COLLECTIONS':
       return { collections: action.payload };
