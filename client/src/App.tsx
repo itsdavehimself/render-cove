a2d603f9c19161d5091465fb99dd0f3d8d848f67
David <23064280+itsdavehimself@users.noreply.github.com>
2024-04-04 09:27:14 -0500

    Delete import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './containers/Home/Home';
 import SignUp from './containers/SignUp/SignUp';
