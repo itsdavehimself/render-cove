b76920cbe6e3be5c41f19fccf5a3a2065f8e352e
David <23064280+itsdavehimself@users.noreply.github.com>
2024-04-08 15:28:00 -0500

    Navigate to thread route if selected and resized

--- client/src/containers/Messages/Messages.tsx
+++ client/src/containers/Messages/Messages.tsx
@@ -1,8 +1,30 @@
+import React, { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import AllMessages from '../../components/Messages/AllMessages/AllMessages';
 import styles from './Messages.module.scss';
 import { Outlet } from 'react-router-dom';
 
 const Messages: React.FC = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (
+        location.pathname.startsWith('/messages/') &&
+        window.innerWidth < 768
+      ) {
+        const userIdToMessage = location.pathname.split('/').pop();
+        navigate(`/messages/thread/${userIdToMessage}`);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [location.pathname, navigate]);
+
   return (
     <div className={styles['messages-container']}>
       <aside className={styles['all-messages']}>
